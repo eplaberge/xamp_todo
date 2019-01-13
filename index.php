fcffd3a488b8958db7689120c17d1a4202850fbf
Eric Phillip LaBerge <fo3126@wayne.edu>
2019-01-13 18:15:49 -0500

    Built structure of login page

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>XAMPP ToDo Project::Login</title>
+    <h3>Login:</h3>
+</head>
+<body>
+<form action="index.php">
+    Username:
+    <input type="text" name="username" placeholder="username"><br>
+    Password:
+    <input type="text" name="password" placeholder="password"><br>
+    <input type="submit" value="Submit"><br>
+</form>
+</body>
+</html>
---
+++ post_login.php
@@ -0,0 +1,5 @@
+<!DOCTYPE html>
+<html>
+<head>
+<h3>Hello World!</h3>
+</head>
